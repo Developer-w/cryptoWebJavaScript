25c48e9f3a7918d41c60ee4960938f7dabd4c847
Developer-w <developerwb@protonmail.com>
2021-05-21 19:10:51 -0400

    Actualizacion V3

--- js/inedx.js
+++ js/inedx.js
@@ -1,23 +1,14 @@
 'use strict'
 
-const cryptomonedaSelect = document.querySelector("coin");
 
-document.addEventListener("DOMContentLoaded", () => {
-    consultarCoin();
-})
 
-function consultarCoin() {
-    const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD";
+$.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD/', function(response) {
+    console.log(response)
+});
 
-    fetch(url)
-        .then( respuesta => respuesta.json() )
-        .then( respuesta => console.log(respuesta) )
 
 }
 
 
 
 
-
-// https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD
-// https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD
