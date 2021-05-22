2e0c5c1c500f153c5753e315e627b3a814674752
Developer-w <developerwb@protonmail.com>
2021-05-21 20:23:39 -0400

    V3 Proyecto js

--- js/inedx.js
+++ js/inedx.js
@@ -2,13 +2,24 @@
 
 
 
-$.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD/', function(response) {
-    console.log(response)
-});
 
 
-}
+const revisarAPI = ()=>{
+
+    console.log($.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD', function(response) {
+        
+        
+
+        //$('#price').text(precio)
+        //$('#mktcp').text(Maeketcap)
 
 
+    }))
+
+        
+
+};
+
+$('#revisar').on('click', revisarAPI)
 
 
